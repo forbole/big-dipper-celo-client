c49675b16f7ac4713cf040e64f0f60ebb83ca697
MonikaCat <pusz.monika@gmail.com>
2020-07-10 04:28:05 +0000

    Updated Coin Balance History Chart

--- components/account/CoinBalanceHistory.tsx
+++ components/account/CoinBalanceHistory.tsx
@@ -14,19 +14,19 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Grid from '@material-ui/core/Grid';
 
-const dataY = [{ value: 2000000 }, { value: 4000000 }, { value: 6000000 }, { value: 80000000 }, { value: 6000000 }, { value: 120000000}];
+const dataY = [{ value: 2000000 }, { value: 4000000 }, { value: 6000000 }, { value: 80000000 }, { value: 6000000 }, { value: 120000000 }];
 const data = [
   {
-    name: 'Apr 10', cGLD: 2500000, 
+    name: 'Apr 10', cGLD: 2500000,
   },
   {
-    name: 'Apr 11', cGLD: 6000000, 
+    name: 'Apr 11', cGLD: 6000000,
   },
   {
-    name: 'Apr 12', cGLD: 9800000, 
+    name: 'Apr 12', cGLD: 5400000,
   },
   {
-    name: 'Apr 13', cGLD: 4000000, 
+    name: 'Apr 13', cGLD: 4000000,
   },
   {
     name: 'Apr 14', cGLD: 6200000,
@@ -46,6 +46,9 @@ const useStyles = makeStyles(() => {
       borderRadius: 5,
       overflowY: "auto",
     },
+    container: {
+      justifyContent: "center",
+    }
   };
 });
 
@@ -63,27 +66,26 @@ export default function CoinBalanceHistory() {
         <Typography variant="body1">Coin Balance History</Typography>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails className={classes.root}>
-      <Grid container >
-        <AreaChart
-          width={350}
-          height={250}
-          data={data}
-          margin={{
-            top: 10, right: 0, left: 0, bottom: 0,
-          }}
-          
-          //baseValue={1000000}
-        >
-            <CartesianGrid strokeDasharray="3 3" strokeWidth={1} opacity={0.3}/>
-            <XAxis dataKey="name" />
-            <YAxis tickSize={0.5} />
-          <Tooltip />
+        <Grid container className={classes.container}>
+          <AreaChart
+            width={350}
+            height={250}
+            data={data}
+            margin={{
+              top: 0, right: 0, left: 0, bottom: 0,
+            }}
+
+          >
+            <CartesianGrid strokeDasharray="3 3" strokeWidth={1} opacity={0.3} />
+            <XAxis dataKey="name" tick={{ stroke: "rgba(255, 255, 255, 0.6)", fontSize: 10, fontWeight: 150 }} />
+            <YAxis tickSize={0} tickMargin={10} tick={{ stroke: "rgba(255, 255, 255, 0.6)", fontSize: 10, fontWeight: 150 }} />
+            <Tooltip />
             {/* <Area type="monotone" dataKey="cGLD" stroke="#8884d8" fill="rgba(58, 211, 158, 0.15)" fillOpacity={1} /> */}
             <Area type={cardinal} dataKey="cGLD" stroke="rgba(58, 211, 158, 1)" fill="rgba(58, 211, 158, 0.15)" fillOpacity={1} />
-        </AreaChart>
-      </Grid>
-    </ExpansionPanelDetails>
-    </ExpansionPanel> 
-    );
+          </AreaChart>
+        </Grid>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  );
 
 }
