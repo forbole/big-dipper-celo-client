a027c241619e42d59c998c9354b2ec3182a23f7b
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-07-31 14:58:37 +0800

    Added GET_ACCOUNTS query

--- components/query/Account.ts
+++ components/query/Account.ts
@@ -1,5 +1,19 @@
 import { gql } from "apollo-boost";
 
+export const GET_ACCOUNTS = gql`
+  query Accounts {
+    accounts {
+      page
+    totalCounts
+    accounts{
+      _id
+      address
+      balance
+    }
+    }
+  }
+`;
+
 export const GET_ACCOUNT_DETAILS = gql`
   query Account($address: String!) {
     account(address: $address) {
