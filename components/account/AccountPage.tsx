c74119373b6f949d9d0bdf6eb1ec705978bb729e
Kwun Yeung <kwun@forbole.com>
2020-08-04 21:08:19 +0800

    remove internal transaction components and fix chainQuery condition

--- components/account/AccountPage.tsx
+++ components/account/AccountPage.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import AccountTransactions from "./Transactions";
-import InternalTransactions from "./InternalTransactions";
 import Downtime from "./Downtime";
 import ValidatedBlocks from "./ValidatedBlocks";
 import AddressCard from "./AddressCard";
