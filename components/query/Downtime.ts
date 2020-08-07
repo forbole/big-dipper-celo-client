a71c7e1ddd0c44809ec79a21c84995e58826e852
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-08-07 12:50:15 +0800

    Removed cursor from Downtime query

--- components/query/Downtime.ts
+++ components/query/Downtime.ts
@@ -3,7 +3,6 @@ import { gql } from "@apollo/client";
 export const DOWNTIME = gql`
   query Downtime($address: String!, $pageSize: Int, $page: Int){
       downtime(address: $address , pageSize: $pageSize,  page: $page){
-      cursor
       pageSize
       page
       totalCounts
