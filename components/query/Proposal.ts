1072f389df34d15686d8c1c75e38b587b86411c7
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-08-17 16:28:01 +0800

    Added query to get Proposal List

---
+++ components/query/Proposal.ts
@@ -0,0 +1,28 @@
+import { gql } from "@apollo/client";
+
+export const GET_PROPOSALS = gql`
+  query Proposals($pageSize: Int, $page: Int, $field: String! ) {
+    proposals(pageSize: $pageSize, page: $page, sortBy:{field: $field, order: DESC}) {
+      cursor
+      pageSize
+      page
+      totalCounts
+      hasMore
+      proposals{
+        _id
+        proposalNumber
+        address
+        blockHash
+        blockNumber
+        event
+        logIndex
+        raw
+        removed
+        returnValues
+        signature
+        transactionHash
+        transactionIndex
+      }
+    }
+  }
+`;
