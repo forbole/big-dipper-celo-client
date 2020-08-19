7ce2ae9c14ba5ccdd87808aca43e704ae95bfd9b
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-08-19 15:40:57 +0800

    Added ValidatorGroup query

---
+++ components/query/ValidatorGroup.ts
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+
+
+export const GET_VALIDATOR_GROUPS = gql`
+    query validatorGroups($pageSize: Int, $page: Int){
+    validatorGroups(pageSize: $pageSize page: $page){
+    validatorGroups{
+        _id
+        address
+        affiliates
+        commission
+        lastSlashed
+        members{
+            address
+            name
+        }
+        membersUpdated
+        name
+        nextCommission
+        nextCommissionBlock
+        slashingMultiplier
+  
+	}
+  }
+}
+`;
