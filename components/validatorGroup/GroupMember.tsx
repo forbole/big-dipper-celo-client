872b912f916c6d61f1dd1544fdb5fe40953520f2
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-08-25 15:17:30 +0800

    Linked part of data in GroupMember

--- components/validatorGroup/GroupMember.tsx
+++ components/validatorGroup/GroupMember.tsx
@@ -6,9 +6,6 @@ import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import IconButton from "@material-ui/core/IconButton";
-import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
-import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import { useRouter } from "next/router";
 import gql from "@apollo/client";
 import { useQuery } from "@apollo/client";
@@ -16,7 +13,12 @@ import ContentLoader from "react-content-loader";
 import moment from "moment";
 import LedgerButtons from "../ledger/LedgerButtons";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
-
+import ComponentLoader from '../misc/ComponentLoader';
+import NotAvailable from '../misc/NotAvailable'
+import ErrorMessage from '../misc/ErrorMessage';
+import { GET_VALIDATOR_GROUP } from '../query/Validator'
+import { GET_ACCOUNT_DETAILS } from '../query/Account'
+import BigNumber from 'bignumber.js'
 
 
 
@@ -92,21 +94,39 @@ const useStyles = makeStyles(() => {
         },
         alignRight: {
             alignItems: "right"
+        },
+        membersInfo: {
+            paddingLeft: "1.5rem",
         }
     };
 });
 
-const GroupMember = () => {
+type GroupMemberProps = { address: string };
+
+
+const GroupMember = ({ address }: GroupMemberProps) => {
 
     const classes = useStyles();
+
+    const { loading, error, data } = useQuery(GET_VALIDATOR_GROUP, {
+        variables: { address },
+    });
+
+    const accountData = useQuery(GET_ACCOUNT_DETAILS, {
+        variables: { address },
+    });
+
+    if (loading) return <ComponentLoader />
+    if (error) return <ErrorMessage message={error.message} />
 
     return (
         <Card className={classes.root}>
             <CardContent>
-                <Grid container spacing={1}  className={classes.item}>
+                <Grid container spacing={1} className={classes.item}>
                     <Grid item xs={12} className={classes.member}>
-                        <Typography color="textPrimary" variant="subtitle1" >
-                            Group member (2) <FiberManualRecordIcon className={classes.dotIcon} />
+                        <Typography color="textPrimary" variant="subtitle1" noWrap>
+                            Group member
+                            {data.validatorGroup && data.validatorGroup.members ? ` (${data.validatorGroup.members.length})` : null} < FiberManualRecordIcon className={classes.dotIcon} />
                         </Typography>
                         <Typography color="textPrimary" variant="body1" className={classes.memberNumber} >
                             Elected
@@ -115,80 +135,48 @@ const GroupMember = () => {
                     <Grid item xs={12}>
                         <Divider variant="middle" className={classes.divider} />
                     </Grid>
-                    <Grid item xs={6} className={classes.member}>
-                        <Typography variant="body2" className={classes.memberNumber}>
-                            #1
-            </Typography>
-                        <Link
-                            href="account/[account]/"
-                            as={`account/${1}`}
-                            color="secondary"
-                        >
-                            <Typography variant="body1" >
-                                Vincent Lynch <FiberManualRecordIcon className={classes.dotIcon} />
-                            </Typography>
-                        </Link>
-                    </Grid>
+                    {data.validatorGroup.members.map((row: any, index: number) => {
+                        return (
+                            <>
+                                <Grid item xs={6} className={classes.member}>
+                                    <Typography variant="body2" className={classes.memberNumber}>
+                                        #{index + 1}
+                                    </Typography>
+                                    <Link
+                                        href="account/[account]/"
+                                        as={`account/${1}`}
+                                        color="secondary"
+                                    >
+                                        {row.name ?
+                                            <Typography variant="body1" noWrap>
+                                                {row.name} <FiberManualRecordIcon className={classes.dotIcon} />
+                                            </Typography> : <NotAvailable variant="body1" />}
+                                    </Link>
+                                </Grid>
 
-                    <Grid item xs={6} >
-                        <Typography variant="body1" align="right" color="textPrimary">
-                            1,000 CGLD
-                        </Typography>
-                    </Grid>
+                                <Grid item xs={6} >
+                                    {accountData.data && accountData.data.account && accountData.data.account.totalBalance && accountData.data.account.totalBalance.lockedGold ?
+                                        <Typography variant="body1" align="right" color="textPrimary">
+                                            {new BigNumber(accountData.data.account.totalBalance.lockedGold).toFormat(2)}
+                                        </Typography> : <NotAvailable variant="body1" />}
+                                </Grid>
 
-                    <Grid item xs={6} >
-                        <Typography variant="caption" >
-                            99.8%
+                                <Grid item xs={6} >
+                                    <Typography variant="caption" className={classes.membersInfo}>
+                                        <img src='/images/time.svg' /> 99.8%
+                                        </Typography>
+                                    <Typography variant="caption" className={classes.membersInfo}>
+                                        <img src='/images/memo.svg' />   10.9%
+                                         </Typography>
+                                </Grid>
+                                <Grid item xs={6} >
+                                    <Typography variant="body2" align="right" >
+                                        0.987CGLD
             </Typography>
-                        <Typography variant="caption" >
-                            10.9%
-            </Typography>
-                    </Grid>
-                    <Grid item xs={6} >
-                        <Typography variant="body2" align="right" >
-                            0.987CGLD
-            </Typography>
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <Divider variant="middle" className={classes.divider} />
-                    </Grid>
-
-                    <Grid item xs={6} className={classes.member}>
-                        <Typography variant="body2" className={classes.memberNumber}>
-                            #2
-            </Typography>
-                        <Link
-                            href="account/[account]/"
-                            as={`account/${1}`}
-                            color="secondary"
-                        >
-                            <Typography variant="body1" >
-                                Michelle Clark
-                            </Typography>
-                        </Link>
-                    </Grid>
-
-                    <Grid item xs={6} >
-                        <Typography variant="body1" align="right" color="textPrimary">
-                            1,000 CGLD
-                        </Typography>
-                    </Grid>
-
-                    <Grid item xs={6} >
-                        <Typography variant="caption" >
-                            99.8%
-            </Typography>
-                        <Typography variant="caption" >
-                            10.9%
-            </Typography>
-                    </Grid>
-                    <Grid item xs={6} >
-                        <Typography variant="body2" align="right" >
-                            0.987CGLD
-            </Typography>
-                    </Grid>
-
+                                </Grid>
+                            </>
+                        )
+                    })}
 
                 </Grid>
             </CardContent >
