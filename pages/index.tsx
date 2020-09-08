9c988f83e6bd0a9ad4dee047883990aaef336e15
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-09-08 17:18:50 +0800

    Added Epoch and Validators Group components to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import LatestBlocks from "../components/block/LatestBlocks";
 import LatestTransactions from "../components/transaction/LatestTransactions";
 import Grid from "@material-ui/core/Grid";
 import Ledger from "../components/ledger/Ledger";
+import TokenPrice from "../components/TokenPrice";
+import Epoch from "../components/Epoch";
+import ValidatorsGroups from "../components/ValidatorsGroups";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,18 +24,28 @@ export default function Index() {
   const classes = useStyles();
   return (
     <Layout >
-      <Grid container  className={classes.root}>
+      <Grid container className={classes.root}>
         <Grid container spacing={2}>
-        <Grid item xs={12} >
-          <ChartData />
+          <Grid item xs={12} >
+            <ChartData />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <TokenPrice />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3} >
+            <Epoch />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <ValidatorsGroups />
+          </Grid>
+
+          <Grid item xs={12} lg={6}  >
+            <LatestBlocks pagination={false} displayCard={false} />
+          </Grid>
+          <Grid item xs={12} lg={6}  >
+            <LatestTransactions pagination={false} />
+          </Grid>
         </Grid>
-        <Grid item xs={12} lg={6}  >
-          <LatestBlocks pagination={false} displayCard={false} />
-        </Grid>
-        <Grid item xs={12} lg={6}  >
-          <LatestTransactions pagination={false} />
-        </Grid>
-      </Grid>
       </Grid>
     </Layout>
   );
