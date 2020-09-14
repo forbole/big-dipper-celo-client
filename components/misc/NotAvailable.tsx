b108db8e7599fd05ed80febdce427a806aabf3cc
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-09-14 18:06:49 +0800

    Added color props to NotAvailable

--- components/misc/NotAvailable.tsx
+++ components/misc/NotAvailable.tsx
@@ -3,12 +3,12 @@ import Typography from '@material-ui/core/Typography';
 import MiddleEllipsis from './MiddleEllipsis'
 import { Variant } from '@material-ui/core/styles/createTypography'
 
-type NotAvailableProps = { wrap?: boolean, variant: Variant, className?: string };
+type NotAvailableProps = { wrap?: boolean, variant: Variant, className?: string, color?: "textPrimary" | "textSecondary" };
 
-const NotAvailable = ({ wrap, variant, className }: NotAvailableProps) => {
+const NotAvailable = ({ wrap, variant, className, color }: NotAvailableProps) => {
     if (wrap) {
         return (
-            <Typography variant={variant} className={className}  >
+            <Typography variant={variant} className={className} color={color || "textPrimary"} >
                 <MiddleEllipsis text="Data not available" />
             </Typography>
         )
