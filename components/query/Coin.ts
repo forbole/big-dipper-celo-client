bb800b93f5d84bc29397a53b66a0b78aaa372f80
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-09-21 19:16:43 +0800

    Added Coin query by dates

--- components/query/Coin.ts
+++ components/query/Coin.ts
@@ -21,7 +21,16 @@ export const GET_COIN_HISTORY_BY_NUM_OF_DAYS = gql`
   query CoinHistoryByNumOfDays($days: Int!) {
     coinHistoryByNumOfDays(days: $days) {
     prices
-    market_caps
+    total_volumes
+    }
+  }
+`;
+
+
+export const GET_COIN_HISTORY_BY_DATES = gql`
+  query CoinHistoryByDates($dateFrom: String!, $dateTo: String!) {
+    coinHistoryByDates(dateFrom: $dateFrom, dateTo: $dateTo) {
+    prices
     total_volumes
     }
   }
