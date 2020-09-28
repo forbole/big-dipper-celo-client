8d369b2d46812c5b62afcef4e1bc5130705a1a39
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-09-28 17:14:28 +0800

    Added ledger dependencies

--- next.config.js
+++ next.config.js
@@ -14,6 +14,18 @@ module.exports = {
         rowSmall: 15,
         rowMedium: 30,
         rowLarge: 50,
-        rowXlarge: 100
+        rowXlarge: 100,
+
+    },
+
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+            config.node = {
+                fs: 'empty'
+            }
+        }
+
+        return config
     }
-};
+}
