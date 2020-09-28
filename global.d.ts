10e94c48f62174c30ad4f4ba6a4cec2312dc5f57
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-09-28 17:14:28 +0800

    Added ledger dependencies

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Include any global TypeScript types or modules in this file
+ */
+
+
+declare module "@ledgerhq/hw-app-eth";
+declare module "@ledgerhq/hw-transport-u2f";
+declare module "@ledgerhq/hw-transport-webusb";
+declare module "lib/client-env";
