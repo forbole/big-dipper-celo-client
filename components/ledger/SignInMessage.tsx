cd939ab5a65c4c78577cac8b22079b6e103231ad
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-09-30 18:19:35 +0800

    Implemented SignIn and Logout

--- components/ledger/SignInMessage.tsx
+++ components/ledger/SignInMessage.tsx
@@ -1,10 +1,23 @@
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import React from 'react';
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import ControlButtons from "./ControlButtons";
+import IconButton from '@material-ui/core/IconButton';
+import LedgerCelo from './LedgerCelo'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Alert from '@material-ui/lab/Alert';
+import {
+    deserializeError,
+    DisconnectedDevice,
+    DisconnectedDeviceDuringOperation
+} from "@ledgerhq/errors";
 
 import ControlButtons from './ControlButtons';
 
@@ -20,39 +33,137 @@ const useStyles = makeStyles({
     },
 
     message: {
-        margin: '0 0.5rem 1.5rem 0.5rem'
-    }
+        margin: "0 0.5rem 0.5rem 0.5rem",
+    },
+    errorMessage: {
+        color: "red",
+        textAlign: "center",
+        paddingBottom: "1rem"
+    },
+    loginButton: {
+        background: "rgba(153, 153, 153, 1)",
+        borderRadius: 5,
+        padding: "0.1rem",
+        verticalAlign: "middle",
+        "&:hover, &.Mui-focusVisible": { backgroundColor: "rgba(58, 211, 158, 0.5)" }
+    },
+
 });
 
-const SignInMessage = (): JSX.Element => {
+
+
+const SignInMessage = () => {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState('');
+    const [loading, setLoading] = React.useState(false);
+    const [loggedIn, setLoggedIn] = React.useState(false);
+    const [retry, setRetry] = React.useState(false);
+
+
+
+    const handleLogin = async () => {
+        if (!loggedIn) {
+            setLoading(true);
+            setOpen(true);
+            try {
+                await LedgerCelo.connect()
+                if (await LedgerCelo.connect() === true) {
+                    setErrorMessage("Please accept the connection in your Ledger device")
+                    let userAddress = await LedgerCelo.getAddress()
+                    try {
+                        let ver = await LedgerCelo.getCeloAppVersion()
+                    }
+                    catch (e) {
+                        setErrorMessage(e.message)
+                        console.log(e.message)
+                    }
+                    setOpen(false);
+                    setLoggedIn(true);
+                }
+
+            }
+            catch (e) {
+                setErrorMessage(LedgerCelo.checkLedgerErrors(e.message))
+                setRetry(true)
+            }
+
+        }
+        else {
+            try {
+                LedgerCelo.disconnect();
+                setLoggedIn(false)
+                setOpen(false);
+
+            }
+            catch (e) {
+                setErrorMessage(e.message)
+
+            }
+        }
+    };
+
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <>
-            <DialogTitle id="ledger-dialog-signin-title">
-                <Grid container className={classes.root}>
-                    <Grid item xs={12}>
-                        <Typography variant="h6" noWrap className={classes.title}>
-                            Sign in with Ledger
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </DialogTitle>
-            <DialogContent>
-                <Grid container spacing={1} className={classes.root}>
-                    <DialogContentText id="signin-ledger">
-                        <Grid item xs={12} className={classes.message}>
-                            <Typography variant="body2">
-                                Please make sure your Ledger device is connected and Celo App 1.5.0
-                                or above is opened.
-                            </Typography>
-                        </Grid>
+            <IconButton
+                aria-label="Login"
+                onClick={handleLogin}
+                className={classes.loginButton}
+            >
+                {!loggedIn ? <img src="/images/connect-ledger.svg" /> : <img src="/images/logout.svg" />}
+
+            </IconButton>
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="ledger-dialog"
+                //fullWidth
+                maxWidth="sm"
+            >
+                <DialogTitle id="ledger-dialog-signin-title">
+                    <Grid container className={classes.root}>
                         <Grid item xs={12}>
-                            <ControlButtons />
+                            <Typography
+                                variant="h6"
+                                noWrap
+                                className={classes.title}
+                            >
+                                Sign in with Ledger
+              </Typography>
                         </Grid>
-                    </DialogContentText>
-                </Grid>
-            </DialogContent>
+                        {loading ? <CircularProgress color="secondary" /> : ''}
+
+                    </Grid>
+                </DialogTitle>
+                <DialogContent >
+                    <Grid container spacing={1} className={classes.root}>
+                        <DialogContentText id="signin-ledger">
+
+                            <Grid item xs={12} className={classes.message}>
+                                <Typography variant="body2">
+                                    Please make sure your Ledger device is connected and Celo
+                                    App 1.0.3 or above is opened.
+                  </Typography>
+                            </Grid>
+                            <Grid item xs={12} className={classes.errorMessage}>
+                                <Typography variant="body2">
+                                    {errorMessage}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} >
+                                {retry ? <ControlButtons retry={true} /> : null}
+                            </Grid>
+                        </DialogContentText>
+                    </Grid>
+                </DialogContent>
+            </Dialog>
+
         </>
     );
 };
