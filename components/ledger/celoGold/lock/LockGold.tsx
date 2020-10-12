698688a9cce0a60480055f007ffdcfc765a9873d
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-10-12 17:37:02 +0800

    LockGold Implementation

--- components/ledger/celoGold/lock/LockGold.tsx
+++ components/ledger/celoGold/lock/LockGold.tsx
@@ -4,7 +4,7 @@ import ListSubheader from "@material-ui/core/ListSubheader";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import FormControl from "@material-ui/core/FormControl";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
-import React, { useEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
@@ -14,7 +14,15 @@ import Grid from "@material-ui/core/Grid";
 import Divider from "@material-ui/core/Divider";
 import ControlButtons from '../../ControlButtons'
 import TextField from '@material-ui/core/TextField';
-
+import LockGoldConfirm from './LockGoldConfirm'
+import { GET_ACCOUNT_DETAILS } from '../../../query/Account';
+import { useQuery } from "@apollo/client";
+import ComponentLoader from '../../../misc/ComponentLoader';
+import NotAvailable from '../../../misc/NotAvailable'
+import ErrorMessage from '../../../misc/ErrorMessage';
+import LedgerCelo from './../../LedgerCelo'
+import Login from '../../Login'
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -115,98 +123,141 @@ const useStyles = makeStyles((theme: Theme) =>
             justifyContent: "center",
         },
 
+        errorMessage: {
+            color: "red",
+            textAlign: "center",
+            paddingBottom: "1rem"
+        },
+
+        circularProgress: {
+            textAlign: "center",
+            paddingBottom: "1rem"
+        }
+
     })
 );
 
-const LockGoldDialog = () => {
-    const classes = useStyles();
-
-    return (
-        <FormControl variant="outlined" fullWidth size="small">
-            <TextField id="lock-gold-dialog" label=""
-                InputProps={{
-                    endAdornment: (
-                        <InputAdornment position="end">CELO</InputAdornment>
-                    ),
-                    disableUnderline: true
-                }}
-                defaultValue="0"
-                className={classes.outlinedInput} />
-
-        </FormControl>
-    );
-}
-
-
-const TokenDropdown = () => {
-    const classes = useStyles();
-    let name = "Michelle Clark";
-    let name_2 = "Ada Adams";
-    return (
-        <FormControl fullWidth={true} size="medium">
-            <Select
-                defaultValue=""
-                id="grouped-select"
-                color="primary"
-                className={classes.select}
-                disableUnderline={true}
-                fullWidth={true}
-
-            >
-                <ListSubheader>Accounts:</ListSubheader>
-                <Divider className={classes.divider} />
-
-                <ListSubheader></ListSubheader>
-                <MenuItem value={1}>
-                    <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className={classes.leftPadding}
-                    >
-                        {name}
-                    </Typography>
-                </MenuItem>
-
-                <Divider variant="middle" className={classes.divider} />
-
-                <ListSubheader></ListSubheader>
-
-                <MenuItem value={2}>
-                    <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className={classes.leftPadding}
-                    >
-                        {name_2}
-                    </Typography>
-                </MenuItem>
-            </Select>
-        </FormControl>
-    );
-}
 
 
 const LockGold = () => {
 
     const classes = useStyles();
     const [open, setOpen] = React.useState(false);
-    const [currentUser, setCurrentUser] = React.useState('' || null);
+    const [connected, setConnected] = React.useState(false);
+    const [currentUser, setCurrentUser] = React.useState('');
+    const [nextDialog, setNextDialog] = React.useState(false);
+    const [amount, setAmount] = React.useState('');
+    const [dialogError, setDialogError] = React.useState(false);
+    const [dialogErrorMessage, setDialogErrorMessage] = React.useState('');
+    const address = currentUser;
+    const [ledgerError, setLedgerError] = React.useState(false);
+    const [ledgerErrorMessage, setLedgerErrorMessage] = React.useState('');
+    const [ledgerLoading, setLedgerLoading] = React.useState(false);
 
-    useEffect(() => {
-        let localUser = localStorage.getItem('currentUserAddress');
-        return () => {
-            // @ts-ignore
-            setCurrentUser(localUser)
-        };
-    });
 
     const handleClose = () => {
         setOpen(false);
     };
 
-    const handleLock = () => {
+    const handleLock = async () => {
+        setLedgerError(false)
+        setLedgerErrorMessage("")
         setOpen(true);
+
+        try {
+            setLedgerLoading(true)
+            setLedgerErrorMessage("Connecting...")
+            await LedgerCelo.connect()
+            if (await LedgerCelo.connect() === true) {
+                setLedgerErrorMessage("Please accept the connection in your Ledger device")
+                let userAddress = await LedgerCelo.getAddress()
+                localStorage.setItem('currentUserAddress', userAddress)
+                setCurrentUser(userAddress)
+                setLedgerErrorMessage("")
+                setConnected(true)
+                setLedgerLoading(false)
+                try {
+                    let ver = await LedgerCelo.getCeloAppVersion()
+                }
+                catch (e) {
+                    setLedgerError(true)
+                    setLedgerErrorMessage(LedgerCelo.checkLedgerErrors(e.message))
+                }
+            }
+        }
+        catch (e) {
+            setLedgerError(true)
+            setLedgerLoading(true)
+            setLedgerErrorMessage(LedgerCelo.checkLedgerErrors(e.message))
+        }
+
+
+
     };
+
+    const confirmLock = async () => {
+
+        try {
+            const from = currentUser
+            const lockObject = { amount, from }
+            await LedgerCelo.lockCelo(lockObject)
+        }
+        catch (e) {
+            setLedgerError(true)
+            setLedgerErrorMessage(LedgerCelo.checkLedgerErrors(e.message))
+
+        }
+
+    };
+
+    const checkForInputErrors = (e) => {
+        if (!(parseFloat(e.target.value) > 0)) {
+            setDialogError(true)
+            setDialogErrorMessage("Incorrect format! Please enter CELO amount to lock. ")
+        }
+
+        else {
+            setDialogError(false)
+            setDialogErrorMessage("")
+        }
+    };
+
+
+    useEffect(() => {
+        let localUser = localStorage.getItem('currentUserAddress');
+        let unlockCELOAmount = document.getElementById("lock-gold-amount") as HTMLInputElement
+        let unlockAmount = unlockCELOAmount ? unlockCELOAmount.value : "0";
+        //@ts-ignore
+        setCurrentUser(localUser)
+        setAmount(unlockAmount)
+    });
+
+
+    const lockGoldDialog = () => {
+        return (
+            <FormControl variant="outlined" fullWidth size="small">
+                <TextField id="lock-gold-amount"
+                    error={dialogError}
+                    helperText={dialogErrorMessage}
+                    onChange={(e) => checkForInputErrors(e)}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">CELO</InputAdornment>
+                        ),
+                        disableUnderline: true
+                    }}
+                    className={classes.outlinedInput} />
+
+            </FormControl>
+        );
+    }
+
+    const { loading, error, data } = useQuery(GET_ACCOUNT_DETAILS, {
+        variables: { address },
+    });
+
+    if (loading) return <ComponentLoader />
+    if (error) return <ErrorMessage message={error.message} />
 
     return (
         <>
@@ -257,7 +308,13 @@ const LockGold = () => {
                 </Typography>
                                 </Grid>
                                 <Grid item xs={12} className={classes.bottomPadding}>
-                                    <TokenDropdown />
+                                    <Typography
+                                        variant="body2"
+                                        noWrap
+                                        color="textPrimary"
+                                    >
+                                        {currentUser}
+                                    </Typography>
                                 </Grid>
 
                                 <Grid item xs={12}>
@@ -271,26 +328,44 @@ const LockGold = () => {
                 </Typography>
                                 </Grid>
                                 <Grid item xs={12}>
-                                    <LockGoldDialog />
+                                    {lockGoldDialog()}
                                 </Grid>
-                                <Grid item xs={12} className={classes.bottomPadding}>
+                                <Grid item xs={12}>
                                     <Typography
                                         variant="body2"
                                         noWrap
                                         className={classes.alignRight}
                                     >
-                                        Max {"14.99217479 CELO"}
+                                        Max {data.account && data.account.totalBalance && data.account.totalBalance.gold ?
+                                            data.account.totalBalance.gold
+                                            : 0} CELO
                                     </Typography>
                                 </Grid>
 
-                                <ControlButtons />
+                                {ledgerLoading ?
+                                    <Grid item xs={12} className={classes.circularProgress}>
+                                        <CircularProgress color="secondary" />
+                                    </Grid>
+                                    : null}
 
+                                {ledgerErrorMessage ?
+                                    <>
+                                        {/* <CircularProgress color="secondary" /> */}
+                                        <Grid item xs={12} className={classes.errorMessage}>
+                                            <Typography variant="body2">
+                                                {ledgerErrorMessage}
+                                            </Typography>
+                                        </Grid> </> : null}
+                                {!ledgerErrorMessage ?
+                                    <ControlButtons handleClick={confirmLock} handleClose={handleClose} showDisabled={dialogError || !connected} />
+                                    :
+                                    <ControlButtons showRetry={true} handleClick={handleLock} handleClose={handleClose} />}
                             </Grid>
                         </DialogContentText>
                     </Grid>
                 </DialogContent>
             </Dialog>
-
+            { nextDialog ? <LockGoldConfirm isOpen={nextDialog} amount={amount} errorMessage={ledgerErrorMessage} /> : null}
         </>
     );
 };
