7d22e4e11073466d6d27c31173f97fa9090c18f8
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-10-29 19:16:48 +0800

    Fixed Bottom Navigation warnings (#103)
    * Fixed BottomNavigation warnings
    
    * Increased the margin

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -1,38 +1,13 @@
-import { Slide, useScrollTrigger } from '@material-ui/core';
+import { Grid, Slide, useScrollTrigger } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
+import Typography from '@material-ui/core/Typography';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import React from 'react';
 
 import NavLink from './NavLink';
-
-// interface TabPanelProps {
-//     children?: React.ReactNode;
-//     index: any;
-//     value: any;
-// }
-
-// function TabPanel(props: TabPanelProps) {
-//     const { children, value, index, ...other } = props;
-
-//     return (
-//         <div
-//             role="tabpanel"
-//             hidden={value !== index}
-//             id={`bottom-navigation-${index}`}
-//             aria-labelledby={`bottom-navigation-${index}`}
-//             {...other}
-//         >
-//             {value === index && (
-//                 <Box p={3}>
-//                     <Typography>{children}</Typography>
-//                 </Box>
-//             )}
-//         </div>
-//     );
-// }
 
 interface TabPanelProps {
     children?: React.ReactNode;
@@ -40,25 +15,33 @@ interface TabPanelProps {
     value: any;
 }
 
-// function TabPanel(props: TabPanelProps) {
-//     const { children, value, index, ...other } = props;
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
 
-//     return (
-//         <div
-//             role="tabpanel"
-//             hidden={value !== index}
-//             id={`bottom-navigation-${index}`}
-//             aria-labelledby={`bottom-navigation-${index}`}
-//             {...other}
-//         >
-//             {value === index && (
-//                 <Box p={3}>
-//                     <Typography>{children}</Typography>
-//                 </Box>
-//             )}
-//         </div>
-//     );
-// }
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`bottom-navigation-${index}`}
+            aria-labelledby={`bottom-navigation-${index}`}
+            {...other}>
+            {value === index && (
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Typography>{children}</Typography>
+                    </Grid>
+                </Grid>
+            )}
+        </div>
+    );
+}
+
+function bottomNav(index: any) {
+    return {
+        id: `bottom-nav-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`
+    };
+}
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
@@ -70,17 +53,17 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
 
     tabElement: {
-        margin: '-0.1rem',
+        margin: '-0.5rem',
         display: 'flex'
     }
 }));
 
 export default function BottomNavigation(): JSX.Element {
     const classes = useStyles();
-    const [value, setValue] = React.useState('');
+    const [value, setValue] = React.useState<number>(0);
     const trigger = useScrollTrigger();
 
-    const handleChange = (event: React.ChangeEvent<{ value: unknown }>, newValue: string) => {
+    const handleChange = (event: unknown, newValue: number) => {
         setValue(newValue);
     };
 
@@ -92,71 +75,134 @@ export default function BottomNavigation(): JSX.Element {
             <div className={classes.root}>
                 <Slide appear={false} direction="up" in={!trigger}>
                     <AppBar position="fixed" color="primary" style={{ top: 'auto', bottom: 0 }}>
-                        <Tabs value={value} scrollButtons="off" aria-label="Celo Bottom Navigation">
-                            <NavLink
-                                href="/"
-                                name={
-                                    <Tab
-                                        icon={<img src="/images/home.svg" alt="Dashboard" />}
-                                        aria-label="Dashboard"
-                                        className={classes.tabElement}
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            scrollButtons="off"
+                            aria-label="Celo Bottom Navigation">
+                            <Tab
+                                icon={
+                                    <NavLink
+                                        href="/"
+                                        name={
+                                            <Tab
+                                                icon={
+                                                    <img src="/images/home.svg" alt="Dashboard" />
+                                                }
+                                                aria-label="Dashboard"
+                                            />
+                                        }
                                     />
                                 }
-                            />
-                            <NavLink
-                                href="/blocks"
-                                name={
-                                    <Tab
-                                        icon={<img src="/images/blocks.svg" alt="Blocks" />}
-                                        aria-label="Blocks"
-                                        className={classes.tabElement}
-                                    />
-                                }
-                            />
-                            <NavLink
-                                href="/transactions"
-                                name={
-                                    <Tab
-                                        icon={<img src="/images/txs.svg" alt="Transactions" />}
-                                        aria-label="Transactions"
-                                        className={classes.tabElement}
-                                    />
-                                }
+                                {...bottomNav(0)}
+                                className={classes.tabElement}
                             />
 
-                            <NavLink
-                                href="/accounts"
-                                name={
-                                    <Tab
-                                        icon={<img src="/images/validators.svg" alt="Accounts" />}
-                                        aria-label="Accounts"
-                                        className={classes.tabElement}
+                            <Tab
+                                icon={
+                                    <NavLink
+                                        href="/blocks"
+                                        name={
+                                            <Tab
+                                                icon={<img src="/images/blocks.svg" alt="Blocks" />}
+                                                aria-label="Blocks"
+                                            />
+                                        }
                                     />
                                 }
+                                {...bottomNav(1)}
+                                className={classes.tabElement}
                             />
-                            <NavLink
-                                href="/proposals"
-                                name={
-                                    <Tab
-                                        icon={<img src="/images/proposal.svg" alt="Proposals" />}
-                                        aria-label="Proposals"
-                                        className={classes.tabElement}
+
+                            <Tab
+                                icon={
+                                    <NavLink
+                                        href="/transactions"
+                                        name={
+                                            <Tab
+                                                icon={
+                                                    <img src="/images/txs.svg" alt="Transactions" />
+                                                }
+                                                aria-label="Transactions"
+                                            />
+                                        }
                                     />
                                 }
+                                {...bottomNav(2)}
+                                className={classes.tabElement}
                             />
-                            <NavLink
-                                href="/validatorVotes"
-                                name={
-                                    <Tab
-                                        icon={<img src="/images/vote.svg" alt="Validator Votes" />}
-                                        aria-label="ValidatorVotes"
-                                        className={classes.tabElement}
+
+                            <Tab
+                                icon={
+                                    <NavLink
+                                        href="/accounts"
+                                        name={
+                                            <Tab
+                                                icon={
+                                                    <img
+                                                        src="/images/validators.svg"
+                                                        alt="Accounts"
+                                                    />
+                                                }
+                                                aria-label="Accounts"
+                                            />
+                                        }
                                     />
                                 }
+                                {...bottomNav(3)}
+                                className={classes.tabElement}
+                            />
+
+                            <Tab
+                                icon={
+                                    <NavLink
+                                        href="/proposals"
+                                        name={
+                                            <Tab
+                                                icon={
+                                                    <img
+                                                        src="/images/proposal.svg"
+                                                        alt="Proposals"
+                                                    />
+                                                }
+                                                aria-label="Proposals"
+                                            />
+                                        }
+                                    />
+                                }
+                                {...bottomNav(4)}
+                                className={classes.tabElement}
+                            />
+
+                            <Tab
+                                icon={
+                                    <NavLink
+                                        href="/validatorVotes"
+                                        name={
+                                            <Tab
+                                                icon={
+                                                    <img
+                                                        src="/images/vote.svg"
+                                                        alt="Validator Votes"
+                                                    />
+                                                }
+                                                aria-label="ValidatorVotes"
+                                            />
+                                        }
+                                    />
+                                }
+                                {...bottomNav(5)}
+                                className={classes.tabElement}
                             />
                         </Tabs>
                     </AppBar>
                 </Slide>
+                <TabPanel value={value} index={0}></TabPanel>
+                <TabPanel value={value} index={1}></TabPanel>
+                <TabPanel value={value} index={2}></TabPanel>
+                <TabPanel value={value} index={3}></TabPanel>
+                <TabPanel value={value} index={4}></TabPanel>
+                <TabPanel value={value} index={5}></TabPanel>
             </div>
         );
     } else {
