449b8da0d0b66e188c81dc754659084bfc5aee1e
Magic Cat <pusz.monika@gmail.com>
2020-11-11 15:01:11 +0800

    Updated Revoke and Confirm Revoke Validator Groups Vote

--- components/ledger/validatorGroup/revoke/Confirm.tsx
+++ components/ledger/validatorGroup/revoke/Confirm.tsx
@@ -5,27 +5,18 @@ import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const useStyles = makeStyles({
     root: {
         justifyContent: 'center'
-    },
-    title: {
-        display: 'block',
-        textAlign: 'center',
-        paddingTop: '0.5rem'
-    },
-
-    dialogTitle: {
-        padding: '1rem'
     },
 
     dialogContent: {
         display: 'flex'
     },
     divider: {
-        margin: '0.8125rem 0rem',
+        marginTop: '0.5rem',
         backgroundColor: 'rgba(232, 232, 232, 1)'
     },
 
@@ -37,120 +28,134 @@ const useStyles = makeStyles({
         justifyContent: 'center'
     },
 
-    wrapText: {
-        wordWrap: 'break-word',
-        wordBreak: 'break-all'
+    signLedgerMessage: {
+        marginTop: '1rem',
+        marginBottom: '-0.8rem'
     },
 
-    centerContent: {
+    alignLeft: {
         display: 'flex',
-        justifyContent: 'center'
+        overflow: 'auto',
+        paddingTop: '0.5rem',
+        paddingBottom: '0.5rem'
     },
-    iconButtonRight: {
-        padding: '0',
-        marginLeft: '0.5rem'
-    },
-    iconButtonLeft: {
-        marginLeft: '-0.6rem'
+    alignRight: {
+        display: 'block',
+        float: 'right',
+        paddingTop: '0.5rem',
+        paddingBottom: '0.5rem'
     }
 });
+type RevokeValGroupConfirmProps = { revokeAmount: string; validatorGroup?: string };
 
-type ConfirmProps = { revokeAmount: string };
-
-const Confirm = ({ revokeAmount }: ConfirmProps): JSX.Element => {
+const Confirm = ({ revokeAmount, validatorGroup }: RevokeValGroupConfirmProps): JSX.Element => {
     const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
+    const [currentUser, setCurrentUser] = React.useState('');
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+    useEffect(() => {
+        const localUser = localStorage.getItem('currentUserAddress');
+        const getLocalUser = localUser ? localUser : '';
+        setCurrentUser(getLocalUser);
+    });
 
     return (
         <>
             <DialogContent>
                 <Grid container spacing={1}>
-                    <DialogContentText id="ledger-vote" className={classes.dialog}>
+                    <DialogContentText id="ledger-lock-gold-confirm" className={classes.dialog}>
                         <Grid container className={classes.dialogContent}>
                             <Grid item xs={12}>
                                 <Typography
-                                    color="textPrimary"
                                     variant="body2"
-                                    gutterBottom
-                                    align="left">
-                                    You’re going to revoke {revokeAmount} Locked CELO to Emma
-                                    Garrett, if that’s correct, please sign in your ledger device.
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Divider variant="middle" className={classes.divider} />
-                            </Grid>
-                            <Grid item xs={6} className={classes.item}>
-                                <Typography variant="body2" color="textPrimary" gutterBottom>
-                                    Account
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={6} className={classes.item}>
-                                <Typography
-                                    variant="body2"
+                                    noWrap={false}
                                     color="textPrimary"
-                                    align="right"
                                     gutterBottom>
-                                    Michelle Clark
+                                    You’re going to revoke {revokeAmount} CELO from {validatorGroup}
+                                    , if that is correct, please sign in your ledger device.
                                 </Typography>
                             </Grid>
                             <Grid item xs={12}>
-                                <Divider variant="middle" className={classes.divider} />
+                                <Divider className={classes.divider} />
                             </Grid>
-
-                            <Grid item xs={6} className={classes.item}>
-                                <Typography variant="body2" color="textPrimary" gutterBottom>
-                                    Validator group
-                                </Typography>
+                            <Grid container spacing={1} className={classes.item}>
+                                <Grid item xs={3}>
+                                    <Typography
+                                        variant="body2"
+                                        noWrap
+                                        className={classes.alignLeft}
+                                        align="left"
+                                        color="textPrimary">
+                                        Account
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={9}>
+                                    <Typography
+                                        variant="body2"
+                                        noWrap
+                                        className={classes.alignRight}
+                                        align="right"
+                                        color="textPrimary">
+                                        {currentUser}
+                                    </Typography>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={6} className={classes.item}>
-                                <Typography
-                                    variant="body2"
-                                    color="textPrimary"
-                                    align="right"
-                                    gutterBottom>
-                                    Nan Valdez Group
-                                    {/* {data.block &&
-              data.block.transactions &&
-              data.block.transactions.transactionIndex
-                ? data.block.transactions.transactionIndex.length()
-                : <NotAvailable variant="body2" />} */}
-                                </Typography>
-                            </Grid>
-
                             <Grid item xs={12}>
-                                <Divider variant="middle" className={classes.divider} />
+                                <Divider className={classes.divider} />
                             </Grid>
-
-                            <Grid item xs={6} className={classes.item}>
-                                <Typography variant="body2" color="textPrimary" gutterBottom>
-                                    Amount
-                                </Typography>
+                            <Grid container spacing={1} className={classes.item}>
+                                <Grid item xs={4}>
+                                    <Typography
+                                        variant="body2"
+                                        noWrap
+                                        className={classes.alignLeft}
+                                        align="left"
+                                        color="textPrimary">
+                                        Validator Group
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={8}>
+                                    <Typography
+                                        variant="body2"
+                                        noWrap
+                                        className={classes.alignRight}
+                                        align="right"
+                                        color="textPrimary">
+                                        {validatorGroup}
+                                    </Typography>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={6} className={classes.item}>
-                                <Typography
-                                    variant="body2"
-                                    color="textPrimary"
-                                    align="right"
-                                    gutterBottom>
-                                    1 Locked CELO
-                                    {/* {data.block && data.block.size
-                ? data.block.size
-                : <NotAvailable variant="body2" />} */}
-                                </Typography>
-                            </Grid>
-
                             <Grid item xs={12}>
-                                <Divider variant="middle" className={classes.divider} />
+                                <Divider className={classes.divider} />
+                            </Grid>
+                            <Grid container spacing={1} className={classes.item}>
+                                <Grid item xs={6}>
+                                    <Typography
+                                        variant="body2"
+                                        noWrap
+                                        className={classes.alignLeft}
+                                        align="left"
+                                        color="textPrimary">
+                                        Amount
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography
+                                        variant="body2"
+                                        noWrap
+                                        className={classes.alignRight}
+                                        align="right"
+                                        color="textPrimary">
+                                        {revokeAmount} CELO
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Divider className={classes.divider} />
                             </Grid>
 
-                            <Grid item xs={12} className={classes.centerContent}>
-                                <Typography variant="h6" color="textPrimary">
-                                    Please sign in your ledger device…
+                            <Grid item xs={12} className={classes.signLedgerMessage}>
+                                <Typography variant="h6" noWrap align="center" color="textPrimary">
+                                    Please sign in your ledger device...
                                 </Typography>
                             </Grid>
                         </Grid>
