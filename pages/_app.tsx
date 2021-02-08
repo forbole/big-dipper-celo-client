1242f66e51fd0df4d7a9466411b4112e2647a1bb
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2021-02-08 17:39:32 +0800

    Removed GraphqlErrors and updated ApolloClient (#210)
    * Removed retryLink, authLink, wsLink, httpLink, errorLink
    
    * Updated CHANGELOG.md

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,5 @@
-import { ApolloClient, ApolloLink, ApolloProvider } from '@apollo/client';
-import { HttpLink, split } from '@apollo/client';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
 import { InMemoryCache } from '@apollo/client/cache';
-import { BatchHttpLink } from '@apollo/client/link/batch-http';
-import { onError } from '@apollo/client/link/error';
-import { RetryLink } from '@apollo/client/link/retry';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { getMainDefinition } from '@apollo/client/utilities';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { init } from '@socialgouv/matomo-next';
@@ -20,71 +14,12 @@ import DarkTheme from '../themes/celo-theme';
 
 const MATOMO_URL = 'https://analytics.bigdipper.live/';
 const MATOMO_SITE_ID = 3;
-const retryLink = new RetryLink();
-
-const authLink = new BatchHttpLink({ uri: 'https://server.celo.bigdipper.live/graphql' });
-
-const wsLink = process.browser
-    ? new WebSocketLink({
-          // only instantiate in the browser
-          uri: `wss://server.celo.bigdipper.live/graphql`,
-          options: {
-              reconnect: true,
-              lazy: true
-          }
-      })
-    : (null as any);
-
-const httplink = new HttpLink({
-    uri: 'https://server.celo.bigdipper.live/graphql',
-    credentials: 'same-origin'
-});
-
-interface DefinintionProps {
-    kind: string;
-    operation?: string;
-}
-
-const errorLink = onError((error) => {
-    const { graphQLErrors, networkError } = error;
-
-    if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
-            console.log(
-                `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(
-                    locations
-                )}, Path: ${path}`
-            )
-        );
-    if (networkError) console.log(`[Network error]: ${networkError}`, networkError);
-});
-
-const link = process.browser
-    ? split(
-          //only create the split in the browser
-          // split based on operation type
-          ({ query }) => {
-              const { kind, operation }: DefinintionProps = getMainDefinition(query);
-              return kind === 'OperationDefinition' && operation === 'subscription';
-          },
-          wsLink,
-          httplink
-      )
-    : httplink;
-
-const ssrMode = typeof window === 'undefined';
 
 const cache = new InMemoryCache({ possibleTypes });
 
 export const client = new ApolloClient({
-    ssrMode,
-    cache,
-    link: ApolloLink.from([errorLink, retryLink, authLink]),
-    defaultOptions: {
-        watchQuery: {
-            fetchPolicy: 'cache-and-network'
-        }
-    }
+    uri: 'https://server.celo.bigdipper.live/graphql',
+    cache
 });
 
 export default function App(props: { Component: any; pageProps: any }): JSX.Element {
