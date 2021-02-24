02a5283a7156bc0d75936d13786a9fc68332194b
Magic Cat <pusz.monika@gmail.com>
2021-02-24 16:53:32 +0800

    Updated utils components

--- components/Utils/MiddleEllipsis.tsx
+++ components/Utils/MiddleEllipsis.tsx
@@ -8,7 +8,7 @@ const MiddleEllipsis = ({ text }: AppProps): JSX.Element => {
     const xs = useMediaQuery(theme.breakpoints.up('xs') && theme.breakpoints.down('sm'));
     const sm = useMediaQuery(theme.breakpoints.up('sm') && theme.breakpoints.down('md'));
     const md = useMediaQuery(theme.breakpoints.up('md') && theme.breakpoints.down('lg'));
-    const lg = useMediaQuery(theme.breakpoints.up('lg') && theme.breakpoints.down('xl'));
+    const lg = useMediaQuery(theme.breakpoints.up('lg'));
 
     const str = text;
 
@@ -16,7 +16,7 @@ const MiddleEllipsis = ({ text }: AppProps): JSX.Element => {
         if (str.length > 10) {
             return (
                 <>
-                    {str.substr(0, 5)}...{str.substr(str.length - 18, str.length)}
+                    {str.substr(0, 5)}...{str.substr(str.length - 5, str.length)}
                 </>
             );
         } else {
@@ -26,7 +26,7 @@ const MiddleEllipsis = ({ text }: AppProps): JSX.Element => {
         if (str.length > 12) {
             return (
                 <>
-                    {str.substr(0, 6)}...{str.substr(str.length - 20, str.length)}
+                    {str.substr(0, 6)}...{str.substr(str.length - 15, str.length)}
                 </>
             );
         } else {
@@ -36,7 +36,7 @@ const MiddleEllipsis = ({ text }: AppProps): JSX.Element => {
         if (str.length > 15) {
             return (
                 <>
-                    {str.substr(0, 7)}...{str.substr(str.length - 15, str.length)}
+                    {str.substr(0, 7)}...{str.substr(str.length - 22, str.length)}
                 </>
             );
         } else {
@@ -46,7 +46,7 @@ const MiddleEllipsis = ({ text }: AppProps): JSX.Element => {
         if (str.length > 17) {
             return (
                 <>
-                    {str.substr(0, 8)}...{str.substr(str.length - 5, str.length)}
+                    {str.substr(0, 8)}...{str.substr(str.length - 25, str.length)}
                 </>
             );
         } else {
